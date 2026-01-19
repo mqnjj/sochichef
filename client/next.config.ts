aea512f2a9a85f07c3d92bcc7b355b3e029be203
Reqox <anovikova999666@gmail.com>
2026-01-20 00:32:14 +0300

    Настроил конфиг. Добавил стили. Убрал лишнее.

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'standalone',
+  sassOptions: {
+    prependData: `@use "@/shared/styles/helpers" as *;`,
+  },
 };
 
 export default nextConfig;
