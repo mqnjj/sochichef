fe3db89e3d8eef8e774b2d970fdccf0502bf28a4
Reqox <anovikova999666@gmail.com>
2026-01-30 16:23:26 +0300

    убрал standalone

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'standalone',
   sassOptions: {
     prependData: `@use "@/shared/styles/helpers" as *;`,
   },
